93fadd6270b8fadba33125276abda23f259dffa3
Manuel <manuelballesteros9312@gmail.com>
2021-11-29 10:29:20 -0500

    Componentes Lazy Agregados

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,9 @@
+
+
+export const LazyPage2 = () => {
+    return (
+        <h2>
+           LazyLoad2
+        </h2>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,9 @@
+
+
+export const LazyPage3 = () => {
+    return (
+        <h2>
+           LazyLoad3
+        </h2>
+    )
+}
